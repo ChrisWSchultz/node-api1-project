40112b85787c1dcc1d256b065412adf9168e6ec2
Christopher W Schultz <chris.william.schultz@gmail.com>
2020-12-02 17:53:50 -0700

    homework

--- routes/users.js
+++ routes/users.js
@@ -6,59 +6,80 @@ const router = express.Router()
 let users = []
 
 router.get('/users', async (request, response, next) => {
-    response.status(200).json(users)
+    try {
+        response.status(200).json(users)
+    } catch (error) {
+        response.status(500).json({"message": "an error occured"})
+    }
 })
 
 router.get('/users/:id', async (request, response, next) => {
-    let id = request.params.id
-    let result = users.filter((user) => {return user.id === id})
+    try {
+        let id = request.params.id
+        let result = users.filter((user) => {return user.id === id})
 
-    response.status(200).json(result)
+        response.status(200).json(result)
+    } catch (error) {
+        response.status(500).json({"message": "an error occured"})
+    }
+
 })
 
 router.post('/users', async (request, response, next) => {
-    let user = {
-        id: nanoid.nanoid(6),
-        name: request.body.name,
-        bio: request.body.bio
-    }
+    try {
+        let user = {
+            id: nanoid.nanoid(6),
+            name: request.body.name,
+            bio: request.body.bio
+        }
 
-    if (user.name && user.bio) {
-        users.push(user)
-        response.status(200).json(users)
-    } else {
-        response.status(400).json({"message": "name and bio are required"})
+        if (user.name && user.bio) {
+            users.push(user)
+            response.status(200).json(users)
+        } else {
+            response.status(400).json({"message": "name and bio are required"})
+        }
+    } catch (error) {
+        response.status(500).json({"message": "an error occured"})
     }
 })
 
 router.put('/users/:id', async (request, response, next) => {
-    let id = request.params.id
+    try {
+        let id = request.params.id
 
-    if (request.body.name && request.body.bio) {
-        let result = users.filter((user) => {return user.id === id})[0]
+        if (request.body.name && request.body.bio) {
+            let result = users.filter((user) => {return user.id === id})[0]
 
-        if (result) {
-            users = users.filter((user) => {return user.id !== id})
-            result = {id: result.id, name: request.body.name, bio: request.body.bio}
-            users.push(result)
-            response.status(200).json(users)
+            if (result) {
+                users = users.filter((user) => {return user.id !== id})
+                result = {id: result.id, name: request.body.name, bio: request.body.bio}
+                users.push(result)
+                response.status(200).json(users)
+            } else {
+                response.status(404).json({"message": "user not found"})
+            }
         } else {
-            response.status(404).json({"message": "user not found"})
+            response.status(400).json({"message": "name and bio are required"})
         }
-    } else {
-        response.status(400).json({"message": "name and bio are required"})
+    } catch (error) {
+        response.status(500).json({"message": "an error occured"})
     }
 })
 
 router.delete('/users/:id', async (request, response, next) => {
-    let id = request.params.id
+    try {
+        let id = request.params.id
 
-    let result = users.filter((user) => {return user.id === id})[0]
-    if (result) {
-        users = users.filter((user) => {return user.id !== id})
-        response.status(200).json(users)
-    } else {
-        response.status(404).json({"message": "user not found"})
+        let result = users.filter((user) => {return user.id === id})[0]
+        if (result) {
+            users = users.filter((user) => {return user.id !== id})
+            response.status(200).json(users)
+        } else {
+            response.status(404).json({"message": "user not found"})
+        }
+    } catch (error) {
+        response.status(500).json({"message": "an error occured"})
     }
 })
 
